f010fa137438bc5f96b2468944345767f53c95dd
NazaniGit <paulohmotta20111@gmail.com>
2025-10-27 12:47:11 -0300

    update main.py to count words

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         text = f.read()
-    return(text)
+    return text
+
+def word_count(text):
+    words = text.split()
+    return len(words)
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
-main() 
+    text = get_book_text("books/frankenstein.txt")
+    word_number = word_count(text)
+    print(f"Found {word_number} total words")
+    
+main()
