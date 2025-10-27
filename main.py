5ffe8fe77d2cd9e73c0bf4d86e2732ac54190ec8
NazaniGit <paulohmotta20111@gmail.com>
2025-10-27 12:13:13 -0300

    added .gitignore and main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        text = f.read()
+    return(text)
+
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    print(book_text)
+main() 
